f6f9681f0625c81bf83a4cf1bf4a6791381ef47f
Svetych <62277992+Svetych@users.noreply.github.com>
2021-04-13 14:58:02 +0300

    Update Gameplay.hs

--- src/Gameplay.hs
+++ src/Gameplay.hs
@@ -4,7 +4,8 @@ import Graphics.Gloss.Interface.Pure.Game
 import Type
 import Field
 import Check
-import Graphic 
+import Graphic
+import Menu
 
 -- файл с головоломкой
 filePath :: FilePath
@@ -15,18 +16,26 @@ fileSave :: FilePath
 fileSave = "save.txt"
 
 -- обновить поле (поменять таймер)
-update :: Float -> Field -> Field
-update _ f = (nextSec f)
-  
+fieldUpdate :: Float -> Field -> Field
+fieldUpdate _ f = (nextSec f)
+
+-- заглушка, реагирует только на событие
+menuUpdate :: Float -> Menu -> Menu
+menuUpdate _ m = m
+
 -- изменение состояния поля
-handleEvent :: Event -> Field -> Field 
-handleEvent (EventKey (SpecialKey KeySpace) Down _ _) f = changeMode f                                                        
-handleEvent (EventKey (MouseButton LeftButton) Down _ mouse) f | x >= 0 && x < width f && y >= 0 && y < height f = changeField f y x
+handleBEvent :: Event -> Field -> Field 
+handleBEvent (EventKey (SpecialKey KeySpace) Down _ _) f = changeMode f                                                        
+handleBEvent (EventKey (MouseButton LeftButton) Down _ mouse) f | x >= 0 && x < width f && y >= 0 && y < height f = changeField f y x
                                                                | otherwise = f
                                                                  where
                                                                    x = mouseToCoordX mouse f
                                                                    y = mouseToCoordY mouse f
-handleEvent _ f = f  
+handleBEvent _ f = f  
+
+-- изменение состояния меню
+handleMEvent :: Event -> Menu -> Menu
+handleMEvent _ m = m
 
 -- Получить координаты клетки под мышкой
 mouseToCoordX :: Point -> Field -> Int
@@ -34,6 +43,20 @@ mouseToCoordX (x, y) f = (floor (x + fromIntegral (screenWidth f) / 2) - (indent
 
 mouseToCoordY :: Point -> Field -> Int
 mouseToCoordY (x, y) f = (height f) - (floor (y + fromIntegral (screenHeight f) / 2) - indent) `div` cellSize -1
+
+playMenu :: Menu -> IO()
+playMenu m = play (display m) bgColor fps m drawMenu handleMEvent menuUpdate
+           where
+            display m = FullScreen
+            bgColor = white
+            fps = 1
+
+playGame :: Field -> IO()
+playGame f = play (display f) bgColor fps f drawGame handleBEvent fieldUpdate
+          where
+            display f = InWindow "Japanese Crosswords" ((screenWidth f), (screenHeight f)) (0, 0)
+            bgColor = white
+            fps = 1
 
 -- программа, что запускает игру и отрисовку поля
 run :: IO ()
@@ -44,13 +67,5 @@ run = do
         putStrLn "Error"
     else do
         let board = readField (lines filecontent) (lines saves) 0 1 0
-        play (display board) bgColor fps board drawGame handleEvent update
-          where
-            display f = InWindow "Japanese Crosswords" ((screenWidth f), (screenHeight f)) (0, 0)
-            bgColor = white
-            fps = 1
-
---  case checkInput filecontent of
---   Nothing -> putStrLn "Parse error"
---    Just cfg -> do
---      play ...
+        playMenu menu
+        playGame board
